0f7efd7daf147f81a118036964bfb5be20b2348c
Alexander Nikolayev <alexnikolayev.dev@gmail.com>
2021-04-12 20:39:19 +0300

    feat: add regex solution to 06-mac-address task

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,11 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const re = /[\dA-F]{2}-[\dA-F]{2}-[\dA-F]{2}-[\dA-F]{2}-[\dA-F]{2}-[\dA-F]{2}/gi;
+  return re.test(n);
 }
+
+isMAC48Address('00-1B-63-84-45-E6');
 
 module.exports = isMAC48Address;
